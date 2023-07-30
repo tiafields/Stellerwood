8552e7e4c4debed6d561ea5af9a9485f2a7b000f
Tia Fields <tia.fields226@gmail.com>
2023-07-30 15:36:04 -0400

    changing file

--- models/User.js
+++ models/User.js
@@ -28,14 +28,6 @@ const userSchema = new mongoose.Schema({
 // ... other code
 
 
-
-// fire a function before doc saved to db
-// userSchema.pre('save', async function(next) {
-  // const salt = await bcrypt.genSalt();
-  // this.password = await bcrypt.hash(this.password, salt);
-  // next();
-// });
-
 // static method to login user
 userSchema.statics.login = async function(email, password) {
   const user = await this.findOne({ email });
